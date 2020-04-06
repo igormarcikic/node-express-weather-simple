1fd8ff1501098e0117423f652091cc1ded47c0d5
Igor <marcikicigor@gmail.com>
2020-04-07 00:00:41 +0200

    Express template render set

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
+const path = require('path');
 const express = require('express');
 
+// Set up paths for app config
+const viewsPath = path.join(__dirname, '../templates/views');
+
+// Initialize express app
 const app = express();
 
+// App config - view engine to handlebars lib and set the default view folder
+app.set('view engine', 'hbs')
+app.set('views', viewsPath);
+
+
 app.get('', (req,res)=>{
-    res.send('Home')
+    res.render('index');
 })
 
+
+// Start up the server on port 3000
 app.listen(3000);
